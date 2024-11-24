d41f251167a384350478e0a5775a64719768d70f
seymennw <seymennw@gmail.com>
2024-11-24 22:36:44 +0100

    Added initial notamDecoder embed

--- src/commands/notams/notamdecoder.ts
+++ src/commands/notams/notamdecoder.ts
@@ -1,6 +1,8 @@
-import { ActionRowBuilder, CommandInteraction, ModalBuilder, ModalSubmitInteraction, SlashCommandBuilder, SlashCommandSubcommandBuilder, TextInputBuilder, TextInputStyle } from 'discord.js';
+import { ActionRowBuilder, CommandInteraction, EmbedBuilder, ModalBuilder, ModalSubmitInteraction, SlashCommandBuilder, SlashCommandSubcommandBuilder, TextInputBuilder, TextInputStyle } from 'discord.js';
 import type { Command } from '../../types/Command';
+//@ts-ignore
 import notamDecoder from '../../services/notams.services/notamdecoder.service';
+import type { DecodedNotam } from '../../types/DecodedNotam';
 
 const storymodal: Command = {
 	data: new SlashCommandBuilder()
@@ -36,10 +38,84 @@ const storymodal: Command = {
 
             
         //@ts-ignore
-        var notam = notamDecoder.decode(notamContentValue)
+        var notam = notamDecoder.decode(notamContentValue) as DecodedNotam;
 
-            modalInteraction.reply(`NOTAM: \n\n${JSON.stringify(notam, null, 4)}`);
-        });
+
+
+        let notamEmbed = new EmbedBuilder();
+
+        let embedError;
+
+        try {
+        notamEmbed
+        .setTitle("NOTAM Information")
+        .setDescription(`Detailed information for NOTAM: ${notam.header.id}`)
+        .addFields(
+          {
+            name: `Header`,
+            value: `ID: ${notam.header.id}\nSeries: ${notam.header.series}\nNumber: ${notam.header.number}\nYear: ${notam.header.year}\nType: ${notam.header.type}\nType Description: ${notam.header.typeDesc}`,
+            inline: false
+          },
+          {
+            name: `Schedule`,
+            value: `Activity Start: ${notam.schedule.activityStart}\nValidity End: ${notam.schedule.validityEnd}`,
+            inline: false
+          },
+          {
+            name: `Location & Coordinates`,
+            value: `Location: ${notam.qualification.location}\nLatitude: ${notam.qualification.coordinates.lat}\nLongitude: ${notam.qualification.coordinates.lng}\nRadius: ${notam.qualification.coordinates.radius}`,
+            inline: false
+          },
+          {
+            name: `Qualification`,
+            value: `Line: ${notam.qualification.code.code}\nEntity: ${notam.qualification.code.entity}\nStatus: ${notam.qualification.code.status}\nArea: ${notam.qualification.code.area}\nSub-Area: ${notam.qualification.code.subArea}\nSubject: ${notam.qualification.code.subject}\nCondition: ${notam.qualification.code.condition}\nModifier: ${notam.qualification.code.modifier}`,
+            inline: false
+          },
+          {
+            name: `Traffic Affected`,
+            value: `${notam.qualification.traffic.map(trf => trf.description).join('\n')}`,
+            inline: false
+          },
+          {
+            name: `Purpose`,
+            value: `${notam.qualification.purpose.map(pur => pur.description).join('\n')}`,
+            inline: true
+          },
+          {
+            name: `Scope`,
+            value: `${notam.qualification.scope.map(sc => sc.description).join(`\n`)}`,
+            inline: true
+          },
+          {
+            name: `Altitude Limits`,
+            value: `Lower: ${notam.qualification.limits.lower}, Upper: ${notam.qualification.limits.upper}`,
+            inline: false
+          },
+          {
+            name: `Content`,
+            value: `${notam.content.text}`,
+            inline: false
+          }
+        )
+        .setColor("#00eb46")
+        .setTimestamp();
+    } catch (err) {
+        embedError = err;
+    }
+            
+        
+       if (embedError){
+        modalInteraction.reply({content: "Invalid NOTAM Provided.\n*only Non-US notams are supported*", ephemeral: true});
+
+       } else {
+        modalInteraction.reply({embeds: [notamEmbed]});
+
+       }
+       
+        
+        
+          
+    });
 
       
 
