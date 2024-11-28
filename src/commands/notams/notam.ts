6baf0a88223be2709ee582cf6cea3be24f14d14b
seymennw <seymennw@gmail.com>
2024-11-28 01:11:12 +0100

    added notam api and types (more work is needed, but it works)

--- src/commands/notams/notam.ts
+++ src/commands/notams/notam.ts
@@ -1,26 +1,116 @@
-import { ActionRowBuilder, CommandInteraction, EmbedBuilder, ModalBuilder, ModalSubmitInteraction, SlashCommandBuilder, SlashCommandSubcommandBuilder, TextInputBuilder, TextInputStyle } from 'discord.js';
+import { ActionRowBuilder, AutocompleteInteraction, CommandInteraction, EmbedBuilder, ModalBuilder, ModalSubmitInteraction, SlashCommandBuilder, SlashCommandSubcommandBuilder, TextInputBuilder, TextInputStyle } from 'discord.js';
 import type { Command } from '../../types/Command';
 import Papa from 'papaparse';
 //@ts-ignore
 import notamDecoder from '../../services/notams.services/notamdecoder.service';
 import type { DecodedNotam } from '../../types/DecodedNotam';
+import type { Notam, NotamList } from '../../types/Notam';
+import getAirportsList from '../../commandServices/airport/getAirportsList';
+import { getCountryNames } from '../../commandServices/country/getCountryName';
+import type { StringChoice } from '../../types/StringChoice';
 
-const storymodal: Command = {
+const notam: Command = {
 	data: new SlashCommandBuilder()
 		.setName('notam')
-		.setDescription('Get the active and upcoming NOTAMs (Notice to Air Missions) for any airport'),
+		.setDescription('Get the active and upcoming NOTAMs (Notice to Air Missions) for any airport')
+		.addStringOption(option => option.setName("airport").setDescription("Choose Airport to view METAR info").setAutocomplete(true).setRequired(true)) as SlashCommandBuilder,
+		
 
 
 	async execute(interaction: CommandInteraction) { 
 
-        interaction.reply("This command has yet to be build (Realtime NOTAM data is not easy to access)");
+		const icaoValue = interaction.options.get('airport')?.value as string;
+
+		const response = await fetch("https://notams.aim.faa.gov/notamSearch/search", {
+			method: "POST",
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			body: 'searchType=0' +
+      `&designatorsForLocation=${icaoValue}` +
+      '&designatorForAccountable=' +
+      '&latDegrees=' +
+      '&latMinutes=0' +
+      '&latSeconds=0' +
+      '&longDegrees=' +
+      '&longMinutes=0' +
+      '&longSeconds=0' +
+      '&radius=10' +
+      '&sortColumns=5+false' +
+      '&sortDirection=true' +
+      '&designatorForNotamNumberSearch=' +
+      '&notamNumber=' +
+      '&radiusSearchOnDesignator=false' +
+      '&radiusSearchDesignator=' +
+      '&latitudeDirection=N' +
+      '&longitudeDirection=W' +
+      '&freeFormText=' +
+      '&flightPathText=' +
+      '&flightPathDivertAirfields=' +
+      '&flightPathBuffer=4' +
+      '&flightPathIncludeNavaids=true' +
+      '&flightPathIncludeArtcc=false' +
+      '&flightPathIncludeTfr=true' +
+      '&flightPathIncludeRegulatory=false' +
+      '&flightPathResultsType=All+NOTAMs' +
+      '&archiveDate=' +
+      '&archiveDesignator=' +
+      '&offset=0' +
+      '&notamsOnly=false' +
+      '&filters=' +
+      '&minRunwayLength=' +
+      '&minRunwayWidth=' +
+      '&runwaySurfaceTypes=' +
+      '&predefinedAbraka=' +
+      '&predefinedDabra=' +
+      '&flightPathAddlBuffer=' +
+      '&recaptchaToken='
+
+		});
+
+		const responseText = await response.text();
+
+		const notams:NotamList = JSON.parse(responseText);
+		
+
+        interaction.reply(notams.notamList[0].icaoMessage);
     
 
 
 	},
-	cooldown: 4
+	cooldown: 4,
+	async autoComplete(interaction: AutocompleteInteraction) {
+        const focusedOption = interaction.options.getFocused(true);
+        let choices: StringChoice[] = [];
+
+      const countryNames = await getCountryNames();
+
+        
+        if (focusedOption.name === 'airport') {
+            
+            for (const airport of await getAirportsList()) {
+
+                const countryName = countryNames.find(cc=>cc.code == airport.iso_country)?.name;
+
+                choices.push({
+                    name: `${airport.name} (${airport.ident}) (${airport.iso_country})`,
+                    searchValue: `${airport.name} (${airport.ident}) (${airport.iso_country}) / ${countryName})`,
+                    value: airport.ident
+                });
+            }
+        }
+
+        const filtered = choices.filter(choice =>
+            choice.searchValue.toLowerCase().includes(focusedOption.value.toLowerCase())
+        ).slice(0,24);
+
+        await interaction.respond(
+            filtered.map(choice => ({ name: choice.name, value: choice.value }))
+        );
+
+    }
 };
 
 
 
-export default storymodal;
+export default notam;
--- src/types/Notam.ts
+++ src/types/Notam.ts
@@ -1,2 +1,58 @@
-//https://pilotweb.nas.faa.gov/PilotWeb/notamRetrievalByICAOAction.do?method=displayByICAOs
-//https://pilotweb.nas.faa.gov/PilotWeb/
+export interface NotamList {
+    notamList: Notam[];
+    startRecordCount: number;
+    endRecordCount: number;
+    totalNotamCount: number;
+    filteredResultCount: number;
+    criteriaCaption: string;
+    searchDateTime: string;
+    linkedLocationCaption: string;
+    error: string;
+    countsByType: CountByType[];
+    requestID: number;
+  }
+  
+ export interface Notam {
+    facilityDesignator: string;
+    notamNumber: string;
+    featureName: string;
+    issueDate: string;
+    startDate: string;
+    endDate: string;
+    source: string;
+    sourceType: string;
+    icaoMessage: string;
+    traditionalMessage: string;
+    plainLanguageMessage: string;
+    traditionalMessageFrom4thWord: string;
+    icaoId: string;
+    accountId: number;
+    airportName: string;
+    procedure: boolean;
+    userID: number;
+    transactionID: number;
+    cancelledOrExpired: boolean;
+    digitalTppLink: boolean;
+    status: string;
+    contractionsExpandedForPlainLanguage: boolean;
+    keyword: string;
+    snowtam: boolean;
+    geometry: string;
+    digitallyTransformed: boolean;
+    messageDisplayed: string;
+    hasHistory: boolean;
+    moreThan300Chars: boolean;
+    showingFullText: boolean;
+    locID: number;
+    defaultIcao: boolean;
+    crossoverTransactionID: number;
+    crossoverAccountID: string;
+    mapPointer: string;
+    requestID: number;
+  }
+  
+ export interface CountByType {
+    name: string;
+    value: number;
+  }
+  
